bf87412789ce44576ab67c7918ddb9c448bfca3d
BuildBot <buildbot@onesystems.ru>
2015-06-14 16:18:53 +0600

    init

---
+++ src/target/ExampleNS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleNS
+//
+//  Created by Nikolay A. Kostin on 11.01.15.
+//  Copyright (c) 2015 Nikolay A. Kostin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/target/ExampleNS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExampleNS
+//
+//  Created by Nikolay A. Kostin on 11.01.15.
+//  Copyright (c) 2015 Nikolay A. Kostin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
